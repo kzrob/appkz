196dd23e123912f1c9de3b706eca6f8541273138
Kevin <davidyo571@gmail.com>
2024-11-22 18:53:03 -0500

    syntax update + working on substring project

--- ruby/basics.rb
+++ ruby/basics.rb
@@ -1,28 +1,65 @@
 def main()
-    caesar_cipher("What a string!", 5)
+    # Caesar Cipher Project
+    puts caesar_cipher("What a string!", 5)
+        #=> "Bmfy f xywnsl!"
+
+
+    # Sub Strings Project
+    _dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    substrings("below", _dictionary)
+        #=> { "below" => 1, "low" => 1 }
+    substrings("Howdy partner, sit down! How's it going?", _dictionary)
+        #=> { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+
+
+    # Stock Picker Project
+    stock_picker()
+
+    # Bubble Sort Project
+    bubble_sort()
 end
 
+$lowAlphabet = [*('a'..'z')]
+$capAlphabet = [*('A'..'Z')]
+$Alphabet = $lowAlphabet + $capAlphabet
+
 def caesar_cipher(string = "", shift = 0)
-    _lows = [*('a'..'z')]
-    _caps = [*('A'..'Z')]
-
-    _chars = string.split("")
+    @chars = string.split("")
     
-    _chars.each_with_index do |char, index|
-        _low = _lows.find_index(char)
-        _cap = _caps.find_index(char)
+    @chars.each_with_index do |char, index|
+        @low = $lowAlphabet.find_index(char)
+        @cap = $capAlphabet.find_index(char)
 
-        if _low != nil then
-            _low = (_low + shift) % _lows.length
-            _chars[index] = _lows[_low]
+        if @low != nil then
+            @low = (@low + shift) % $lowAlphabet.length
+            @chars[index] = $lowAlphabet[@low]
         end
-        if _cap != nil then
-            _cap = (_cap + shift) % _caps.length
-            _chars[index] = _caps[_cap]
+        if @cap != nil then
+            @cap = (@cap + shift) % $capAlphabet.length
+            @chars[index] = $capAlphabet[@cap]
         end 
     end
 
-    puts(_chars.join)
+    return @chars.join
+end
+
+def substrings(string = "", dictionary = {})
+    @counts = {}
+
+    @words = string.split(" ")
+    @words.map do |word|
+        word.select { |char| $Alphabet.include?(char) }
+    end
+    
+    print @words
+end
+
+def stock_picker()
+
+end
+
+def bubble_sort()
+
 end
 
 main()
